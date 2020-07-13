2120cfd32ed18f6810647bafae7ce9ae8165be76
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-07-13 18:13:01 -0400

    Corrections to DTO and Mapping Profile

--- SchoolAPI/MappingProfile.cs
+++ SchoolAPI/MappingProfile.cs
@@ -11,6 +11,9 @@ namespace SchoolAPI.Controllers
             CreateMap<User, UsersDTO>()
                     .ForMember(c => c.FullAddress,
                         opt => opt.MapFrom(x => string.Join(' ', x.UserName, x.Password, x.Email, x.Status, x.SystemRoleID, x.CreatedDate, x.UpdatedDate)));
+
+            CreateMap<UserForCreationDTO, User>();
+            CreateMap<UserForUpdateDTO, User>();
         }
     }
 }
