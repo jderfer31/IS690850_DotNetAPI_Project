e54de75b92f522d4546d853284198ac14abe0b62
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-07-17 17:09:04 -0400

    Added groupName line

--- SchoolAPI/Controllers/UsersController.cs
+++ SchoolAPI/Controllers/UsersController.cs
@@ -10,6 +10,7 @@ namespace SchoolAPI.Controllers
 {
     [Route("api/v1/users")]
     [ApiController]
+    [ApiExplorerSettings(GroupName = "v1")]
     public class UsersController : ControllerBase
     {
         private readonly IRepositoryManager _repository;
