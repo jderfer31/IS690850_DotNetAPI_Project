fb4f1736b3065ba69ea3c25b830f0b2012db8f7e
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-08-04 20:03:53 -0400

    Created Authentication files

---
+++ Contracts/IAuthenticationManagement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.DataTransferObjects;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IAuthenticationManagement
+    {
+        Task<bool> ValidateUser(UserForAuthenticationDto userForAuth);
+        Task<string> CreateToken();
+    }
+}
