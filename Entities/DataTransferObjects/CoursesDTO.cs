657defd69bc4afdf9bb77cf8b989e83a7155687a
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-08-04 23:15:05 -0400

    Task: Added CoursesDTO

---
+++ Entities/DataTransferObjects/CoursesDTO.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Entities.DataTransferObjects
+{
+    public class CourseDto
+    {
+        public Guid id { get; set; }
+        public string CourseName { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+
+
+    }
+}
