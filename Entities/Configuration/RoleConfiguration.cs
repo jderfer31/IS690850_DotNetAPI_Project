51628c48db960d4cf1448536797c697e919b9472
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-08-04 23:10:49 -0400

    Task: Added RoleConfiguration.cs

---
+++ Entities/Configuration/RoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Entities.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+              new IdentityRole
+              {
+                  Name = "Administrator",
+                  NormalizedName = "ADMINISTRATOR"
+              },
+              new IdentityRole
+              {
+                  Name = "Student",
+                  NormalizedName = "STUDENT"
+              }
+            );
+        }
+    }
+}
